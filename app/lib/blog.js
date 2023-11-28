aad3d75adf964563d1b103eeeff7992bbab5f5b8
ARoble <abdulladif0042@gmail.com>
2023-11-28 14:57:26 +0300

    added try catch to fetch

--- app/lib/blog.js
+++ app/lib/blog.js
@@ -6,17 +6,25 @@ const BASE_URL = "https://portfolio-azure-nine-71.vercel.app/api";
 
 export async function getAllBlogs() {
   //TODO: PASS A LIMIT FOR THE HOMEPAGE
-  const data = await fetch(`${BASE_URL}/blog`, {
-    cache: "no-store",
-  });
+  try {
+    const data = await fetch(`${BASE_URL}/blog`, {
+      cache: "no-store",
+    });
 
-  return data.json();
+    return data.json();
+  } catch (e) {
+    console.log("error");
+  }
 }
 
 export async function getOneBlog({ slug }) {
-  const data = await fetch(`${BASE_URL}/blog?slug=${slug}`, {
-    cache: "no-store",
-  });
+  try {
+    const data = await fetch(`${BASE_URL}/blog?slug=${slug}`, {
+      cache: "no-store",
+    });
 
-  return data.json();
+    return data.json();
+  } catch (e) {
+    console.log("error");
+  }
 }
