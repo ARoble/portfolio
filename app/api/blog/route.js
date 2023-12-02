5f30e57c6a4a7b0faac173239fbf8d91d08334ab
ARoble <abdulladif0042@gmail.com>
2023-12-02 09:39:38 +0300

    added test route

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -5,24 +5,26 @@ const client = contentful.createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_KEY,
 });
-0;
+
 export async function GET(req) {
   try {
-    const searchParams = req.nextUrl.searchParams;
-    const slug = searchParams.get("slug");
-    const limit = searchParams.get("limit");
+    // const searchParams = req.nextUrl.searchParams;
+    // const slug = searchParams.get("slug");
+    // const limit = searchParams.get("limit");
 
-    const response = await client.getEntries({
-      content_type: "blog",
-      "fields.slug": slug,
-    });
+    // const response = await client.getEntries({
+    //   content_type: "blog",
+    //   "fields.slug": slug,
+    // });
 
-    let blogs = response.items.map((blog) => {
-      return blog.fields;
-    });
-    if (blogs.length === 1 && slug != null) blogs = blogs[0];
+    // let blogs = response.items.map((blog) => {
+    //   return blog.fields;
+    // });
+    // if (blogs.length === 1 && slug != null) blogs = blogs[0];
 
-    return NextResponse.json({ blogs });
+    const response = await client.getEntries({});
+
+    return NextResponse.json({ response });
   } catch (e) {
     console.log("server", e);
     return NextResponse.json({ message: e });
